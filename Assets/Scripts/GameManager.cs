7f66bf6e193e4d1c3ff4488821c9dca0c613ca84
akoshy21 <ak6429@nyu.edu>
2019-09-05 14:58:54 -0400

    rocket scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,20 +5,80 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public float timer;
+    public static GameManager gm;
+
+    public float timer, currentTime;
+
+    public GameObject rocketPrefab;
 
     public GameObject rocketOne, rocketTwo;
-    public Text scoreOne, scoreTwo;
+    public Text scoreOneText, scoreTwoText;
+    public int scoreOne, scoreTwo;
+
+    public Image fillBar;
+
+    Vector3 posOne, posTwo;
+
+    private void Awake()
+    {
+        if (gm == null)
+        {
+            gm = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (gm != this && gm != null)
+        {
+            Destroy(this);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        posOne = rocketOne.transform.position;
+        posTwo = rocketTwo.transform.position;
+
+        currentTime = timer;
+        InvokeRepeating("UpdateTimerAndFill", 0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreOneText.text = scoreOne.ToString();
+        scoreTwoText.text = scoreTwo.ToString();
+    }
+
+    void UpdateTimerAndFill()
+    {
+        currentTime--;
+
+        fillBar.fillAmount = currentTime / timer;
+    }
+
+    public IEnumerator PlayerOneScore()
+    {
+        scoreOne++;
+        yield return new WaitForSeconds(1);
+        rocketOne = Instantiate(rocketPrefab, posOne, Quaternion.identity);
+    }
+
+    public IEnumerator PlayerOneHit()
+    {
+        yield return new WaitForSeconds(1);
+        rocketOne = Instantiate(rocketPrefab, posOne, Quaternion.identity);
+    }
+
+    public IEnumerator PlayerTwoScore()
+    {
+        scoreOne++;
+        yield return new WaitForSeconds(1);
+        rocketOne = Instantiate(rocketPrefab, posTwo, Quaternion.identity);
+    }
+
+    public IEnumerator PlayerTwoHit()
+    {
+        yield return new WaitForSeconds(1);
+        rocketTwo = Instantiate(rocketPrefab, posTwo, Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public bool rocketOne;
+
+    private void Update()
+    {
+        if (rocketOne)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                this.transform.position += new Vector3(0, 1, 0);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                this.transform.position += new Vector3(0, -1, 0);
+            }
+        }
+        else {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                this.transform.position += new Vector3(0, 1, 0);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                this.transform.position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Bound"))
+        {
+            if (rocketOne)
+            {
+                StartCoroutine(GameManager.gm.PlayerOneScore());
+            }
+            else {
+                StartCoroutine(GameManager.gm.PlayerTwoScore());
+            }
+            Destroy(this);
+        }
+        else if(collision.gameObject.tag.Equals("Asteroid"))
+        {
+            if (rocketOne)
+            {
+                StartCoroutine(GameManager.gm.PlayerOneHit());
+            }
+            else {
+                StartCoroutine(GameManager.gm.PlayerTwoHit());
+            }
+            Destroy(this);
+        }
+    }
+}
